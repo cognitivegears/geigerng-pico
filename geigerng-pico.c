8427d66081b535053d9fb09079b94dc8f1b3c353
Nathan Byrd <techy1@gmail.com>
2025-06-01 20:58:55 -0500

    Updates for integration

--- geigerng-pico.c
+++ geigerng-pico.c
@@ -3,32 +3,49 @@
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 
-#define GEIGER_PIN 2
+#define GEIGER_PIN 20
 
-volatile uint16_t random_value = 0;
+volatile uint16_t current_value = 0;
+volatile uint32_t interrupt_count = 0;
 
 void interrupt_handler()
 {
-    printf("%u\n", random_value);
+    // Acknowledge interrupt first
     gpio_acknowledge_irq(GEIGER_PIN, GPIO_IRQ_EDGE_RISE);
+
+    // Increment counter to trigger the interrupt
+    interrupt_count++;
 }
 
 void init()
 {
     stdio_init_all();
+
     gpio_init(GEIGER_PIN);
     gpio_set_dir(GEIGER_PIN, GPIO_IN);
-    gpio_pull_down(GEIGER_PIN); // Enable pull-down resistor
-    gpio_set_irq_enabled_with_callback(GEIGER_PIN, GPIO_IRQ_EDGE_RISE, true, &interrupt_handler);
+    gpio_pull_down(GEIGER_PIN);
+
+    gpio_set_irq_enabled_with_callback(GEIGER_PIN, GPIO_IRQ_EDGE_RISE,
+        true, &interrupt_handler);
 }
 
 int main()
 {
-    // Initialize the Pico
     init();
 
-    while (true)
-    {
-        random_value++;
+    uint32_t last_interrupt_count = 0;
+    bool last_gpio_state = false;
+
+    while (true) {
+
+        if (interrupt_count != last_interrupt_count) {
+            // Print current_value as a series of binary digits
+            for(int i = 15; i >= 0; i--) {
+                printf("%d", (current_value >> i) & 1);
+            }
+            last_interrupt_count = interrupt_count;
+        }
+
+        current_value++;
     }
 }
